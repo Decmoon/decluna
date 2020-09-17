2747d86adc361757525b1fad7a13c370d3494c05
decmoon <751229863@qq.com>
2020-09-17 15:49:29 +0800

    code optimized for decluna aspect

--- src/main/java/com/decmoon/decluna/config/DecLunaAspect.java
+++ src/main/java/com/decmoon/decluna/config/DecLunaAspect.java
@@ -1,6 +1,6 @@
 package com.decmoon.decluna.config;
 
-import com.decmoon.shortcut.core.config.ShortCutAspect;
+import com.decmoon.shortcut.core.config.ShortcutAspect;
 import com.decmoon.shortcut.core.log.Console;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -8,7 +8,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
-public class DecLunaAspect extends ShortCutAspect {
+public class DecLunaAspect extends ShortcutAspect {
 
     {
         Console.warn("The aspect service of Decluna is started");
